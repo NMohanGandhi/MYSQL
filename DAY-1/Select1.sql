a2645c9e0d37c6df6ff7a80a0e0b74491a327be3
NALLAGANGULA MOHAN GANDHI <92673465+NMohanGandhi@users.noreply.github.com>
2024-03-09 08:37:55 +0530

    Update Select1.sql

--- DAY-1/Select1.sql
+++ DAY-1/Select1.sql
@@ -7,10 +7,12 @@ value('karun','ohfouruiroug@gmail.com','1igf4583fedyfu4#');
 
 select * from users;
 
-select email from users;
+select email from users;  /* select all emails from users table*/
 
-select name,email from users;
+select name,email from users; /* select all names,emails from users table*/
 
+
+/* checking this user email='sreeyadav.com' presents or not */
 select * from users where email='sreeyadav.com';
 
 select * from users where email='sradav.com';
@@ -18,3 +20,4 @@ select * from users where name='sreeyadav.com';
 select * from users where name='yuvasree';
 
 select * from users where name='yuvasree' or password='1245nN84#';
+select * from users where name='yuvasree' and password='1245nN84#';
